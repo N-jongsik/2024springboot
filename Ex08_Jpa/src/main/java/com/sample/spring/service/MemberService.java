083c7e172dd8d40f92473e62a9e2fe0e0353ff5d
N-jongsik <jongsik0625@naver.com>
2024-10-08 15:45:11 +0900

    jpa pagenation

--- Ex08_Jpa/src/main/java/com/sample/spring/service/MemberService.java
+++ Ex08_Jpa/src/main/java/com/sample/spring/service/MemberService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -32,4 +34,10 @@ public class MemberService {
 	public void delete(Long id) {
 		memberRepository.deleteById(id);
 	}
+
+	public Page<Member> selectNameLike(String search, Pageable pageable) {
+		Page<Member> member = memberRepository.findByNameLike(search, pageable);
+		return member;
+	}
+
 }
---
+++ Ex08_Jpa/src/main/webapp/WEB-INF/views/selectList.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@taglib uri="jakarta.tags.core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+총 글의 개수: ${totalElements }<br>
+총 페이지: ${totalPages }<br>
+사이즈: ${size }<br>
+페이지 넘버: ${pageNumber }<br>
+number of element: ${numberOfElements }<br>
+	<c:forEach var="member" items="${members}">
+		${member.name} / ${member.name} /${member.email }
+		<hr>
+	</c:forEach>
+</body>
+</html>
